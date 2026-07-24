6145d1ae8c1344a674e254752ce4e9e3385ea182
682023027 <682023027@student.uksw.edu>
2026-07-24 10:05:22 +0700

    fix vercel parser again

--- index.py
+++ index.py
@@ -9,18 +9,22 @@ atau:
 import sys
 import traceback
 
-try:
-    from app import create_app
-    app = create_app()
-except Exception as e:
-    from flask import Flask
-    app = Flask(__name__)
-    error_msg = traceback.format_exc()
-    
-    @app.route('/', defaults={'path': ''})
-    @app.route('/<path:path>')
-    def catch_all(path):
-        return f"<h1>Failed to initialize Flask App</h1><pre>{error_msg}</pre>", 500
+def initialize_app():
+    try:
+        from app import create_app
+        return create_app()
+    except Exception as e:
+        from flask import Flask
+        dummy_app = Flask(__name__)
+        error_msg = traceback.format_exc()
+        
+        @dummy_app.route('/', defaults={'path': ''})
+        @dummy_app.route('/<path:path>')
+        def catch_all(path):
+            return f"<h1>Failed to initialize Flask App</h1><pre>{error_msg}</pre>", 500
+        return dummy_app
+
+app = initialize_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
